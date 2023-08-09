1594686dfc4ddf271bf4c13fb0fb79db0cf788c6
Hyacinth <hyacinthweng@gmail.com>
2023-08-09 18:14:27 -0400

    added notebook 2

--- notebookPages.js
+++ notebookPages.js
@@ -2,39 +2,38 @@ let image = document.querySelector("#page");
 let more = document.querySelector("#more");
 let slider = document.querySelector("#pageSlider");
 let numText = document.querySelector("#pageNumber");
+let notebookId = document.querySelector("h3").id;
 let pageNum = 0;
 let maxPage = slider.max;
 
-function prevPage(){
-    if(pageNum > 0){
+function prevPage() {
+    if (pageNum > 0) {
         pageNum--;
-        image.src = `./assets/notebook_${pageNum}.jpg`;
-        numText.innerHTML = "Page "+ pageNum;
+        image.src = `./assets/${notebookId}_${pageNum}.jpg`;
+        numText.innerHTML = "Page " + pageNum;
         slider.value = pageNum;
     }
-    
 }
 
-function nextPage(){
-    if(pageNum < maxPage){
+function nextPage() {
+    if (pageNum < maxPage) {
         pageNum++;
-        image.src = `./assets/notebook_${pageNum}.jpg`;
-        numText.innerHTML = "Page "+ pageNum;
+        image.src = `./assets/${notebookId}_${pageNum}.jpg`;
+        numText.innerHTML = "Page " + pageNum;
         slider.value = pageNum;
     }
-    
 }
 
-function toggleMore(){
-    if(more.style.visibility == "visible"){
+function toggleMore() {
+    if (more.style.visibility == "visible") {
         more.style.visibility = "hidden";
-    }else{
+    } else {
         more.style.visibility = "visible";
     }
 }
 
-slider.oninput = function(){
+slider.oninput = function () {
     pageNum = this.value;
-    image.src = `./assets/notebook_${pageNum}.jpg`;
-    numText.innerHTML = "Page "+ this.value;
-}
+    image.src = `./assets/${notebookId}_${pageNum}.jpg`;
+    numText.innerHTML = "Page " + this.value;
+};
